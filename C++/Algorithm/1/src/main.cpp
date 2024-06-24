0524fe07508c0404326cf90384803d90706ecdc2
LDB1208 <LdbRn20101208@126.com>
2024-06-24 17:08:45 +0800

    remove opengl & imgui, preparing for qt

--- C++/Algorithm/1/src/main.cpp
+++ C++/Algorithm/1/src/main.cpp
@@ -6,7 +6,7 @@
  */
 
 template<typename T>
-void selection_sort(std::vector<T> &array) {
+void selection_sort_asc(std::vector<T> &array) {
     for (int i = 0; i < array.size(); ++i) {
         int min = i;
         for (int j = i + 1; j < array.size(); ++j)
@@ -15,16 +15,32 @@ void selection_sort(std::vector<T> &array) {
     }
 }
 
+template<typename T>
+void selection_sort_desc(std::vector<T> &array) {
+    for (int i = 0; i < array.size(); ++i) {
+        int max = i;
+        for (int j = i + 1; j < array.size(); ++j)
+            if (array[j] > array[max]) max = j;
+        std::swap(array[i], array[max]);
+    }
+}
+
 int main() {
     std::vector<char> a = {'a', 'c', 'b', 'a', 'y', 'g'};
-    selection_sort(a);
+    selection_sort_asc(a);
+    for (char i: a)
+        std::cout << i << ' ';
+    selection_sort_desc(a);
     for (char i: a)
         std::cout << i << ' ';
 
     std::cout << '\n';
 
     std::vector<int> b = {35, 6, 23, 10, -3, 19};
-    selection_sort(b);
+    selection_sort_asc(b);
+    for (int i: b)
+        std::cout << i << ' ';
+    selection_sort_desc(b);
     for (int i: b)
         std::cout << i << ' ';
     return 0;
--- C++/OpenGL/Grammar/CONTENTS.md
+++
@@ -1,4 +0,0 @@
-# CONTENTS
-1. 使用GLFW创建一个窗口(绘制三角形)
-2. 使用GLFW和GLEW
-3. 使用现代OpenGL绘制三角形
