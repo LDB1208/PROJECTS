64c9c676119b53bacd4ebcb96b0daa2f4db094cc
LDB1208 <LdbRn20101208@126.com>
2025-01-17 15:51:47 +0800

    visual studio code

--- C++/MyProjects/1+1/src/main.cpp
+++ C++/MyProjects/1+1/src/main.cpp
@@ -3,6 +3,6 @@
 int main(){
     int a[2]{1, 1}, *ptr = a;
     printf("%d", *reinterpret_cast<int *>(reinterpret_cast<char *>(ptr)) +
-                     *reinterpret_cast<int *>(reinterpret_cast<char *>(ptr) + sizeof(int)));
+                 *reinterpret_cast<int *>(reinterpret_cast<char *>(ptr) + sizeof(int)));
     return 0;
 }
